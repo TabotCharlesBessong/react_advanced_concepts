adfd4a8f9245c8fa136be3be0c55c3828fdb0205
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-25 07:27:23 +0100

    feat: (setting up tailwind css)

--- React_Components/src/App.tsx
+++ React_Components/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className='text-center text-3xl text-orange-600' >Hello World</h1>
     </div>
   )
 }
