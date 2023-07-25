2e59bcc775fd49673a2a7ca32ef9825728732c7b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-07-25 07:44:57 +0100

    button components

--- React_Components/src/App.tsx
+++ React_Components/src/App.tsx
@@ -1,9 +1,12 @@
 // import React from 'react'
 
+// import { Button } from "./components/ui/Button"
+
 const App = () => {
   return (
     <div>
       <h1 className='text-center text-3xl text-orange-600' >Hello World</h1>
+      {/* <Button/> */}
     </div>
   )
 }
